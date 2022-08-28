626c86bdf9d8d5d2e1b6b120c8cbc331d155c2bd
Anna Ojdana <anna.ojdana@gmail.com>
2022-08-28 19:10:24 +0200

    ImageGalleryItem component

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,18 +1,31 @@
+import styles from './ImageGalleryItem.module.css';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class ImageGalleryItem extends Component {
-  render() {
-    return (
-      <div>
 
-      </div>
+  render() {
+
+    const { imageData, openModal } = this.props;
+    const { item, image } = styles;
+    const { smallImageUrl, tags, largeImageURL } = imageData;
+
+    return (
+      <li className={item}>
+        <img
+          className={image}
+          src={smallImageUrl}
+          alt={tags}
+          onClick={() => openModal(largeImageURL, tags)}
+        />
+      </li>
     );
   }
 }
 
 ImageGalleryItem.propTypes = {
-
+  imageData: PropTypes.object.isRequired,
+  openModal: PropTypes.func.isRequired,
 };
 
 export default ImageGalleryItem;
