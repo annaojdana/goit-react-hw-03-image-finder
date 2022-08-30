cb7a13e89edbacc5c0d6ca6d0211a1d5d42abd57
Anna Ojdana <anna.ojdana@gmail.com>
2022-08-30 18:12:07 +0200

    Loader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,41 +4,52 @@ import getImages from 'services/getImages';
 import ImageGallery from './ImageGallery/ImageGallery';
 import Searchbar from './Searchbar/Searchbar';
 import { Button } from './Button/Button';
+import Loader from './Loader/Loader';
 
 const INITIAL_STATE = {
   query: '',
   images: [],
   limitImages: 12,
   numberOfHits: 0,
-  modal: false,
+  openModal: false,
+  isLoading: false,
 };
 class App extends Component {
   state = {
     ...INITIAL_STATE,
   };
+  setIsLoading = isLoading =>
+    this.setState(oldState => ({
+      ...oldState,
+      isLoading: isLoading,
+    }));
 
   onSubmit = e => {
     e.preventDefault();
+    this.setIsLoading(true);
     const { queryInput } = e.target.elements;
     const queryValue = queryInput.value;
     const initialImagesLimit = 12;
-    const { limitImages } = this.state;
     getImages(queryValue, initialImagesLimit)
       .then(data => {
-        data.totalHits === 0
-          ? alert(
-              'Sorry, there are no images matching your search query. Please try again.'
-            )
-          : alert(`Hooray! We found ${data.totalHits} images.`);
+        if (data.totalHits === 0) {
+          alert(
+            'Sorry, there are no images matching your search query. Please try again.'
+          );
+        }
+        this.setIsLoading(false);
         return this.setState(oldState => ({
           ...oldState,
           images: data.hits,
           numberOfHits: data.totalHits,
           query: queryValue,
-          limitImages:initialImagesLimit,
+          limitImages: initialImagesLimit,
         }));
       })
-      .catch(error => console.log(error));
+      .catch(error => {
+        this.setIsLoading(false);
+        console.log(error);
+      });
   };
 
   handleModal = (urlLargeImage, altForLargeImage) => {
@@ -47,12 +58,12 @@ class App extends Component {
 
   loadMore = () => {
     const { limitImages, query } = this.state;
-    return getImages(query, limitImages+12)
+    return getImages(query, limitImages + 12)
       .then(data => {
         return this.setState(oldState => ({
           ...oldState,
           images: data.hits,
-          limitImages:limitImages+12,
+          limitImages: limitImages + 12,
         }));
       })
       .catch(error => console.log(error));
@@ -60,21 +71,27 @@ class App extends Component {
 
   render() {
     const { container } = styles;
-    const { images, numberOfHits } = this.state;
+    const { images, numberOfHits, isLoading } = this.state;
     console.log(this.state);
     return (
       <div className={container}>
         <Searchbar onSubmit={this.onSubmit} />
-        {images.length > 0 && (
-          <>
-            <ImageGallery imagesData={images} openModal={this.handleModal} />
-            {images.length < numberOfHits && <Button onClick={this.loadMore} />}
-          </>
+        {isLoading ? (
+          <Loader />
+        ) : (
+          images.length > 0 && (
+            <>
+              <ImageGallery imagesData={images} openModal={this.handleModal} />
+              {images.length < numberOfHits && (
+                <Button onClick={this.loadMore} />
+              )}
+            </>
+          )
         )}
         {}
       </div>
     );
-}
+  }
 }
 
 export default App;
--- src/services/getImages.js
+++ src/services/getImages.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const getImages = async (query, limit) => {
   const apiKey = '2517208-093ee8e26dcc8dc903fe58900';
   try {
-    const response = await axios.get('https://pixabay.com/api/?${params}', {
+    const response = await axios.get("https://pixabay.com/api/?${params}", {
       params: {
         key: apiKey,
         q: query,
