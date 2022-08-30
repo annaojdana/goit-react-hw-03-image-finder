dc86299637565c51024433c2beafb91df23680d6
Anna Ojdana <anna.ojdana@gmail.com>
2022-08-30 19:33:12 +0200

    params

--- src/services/getImages.js
+++ src/services/getImages.js
@@ -2,17 +2,16 @@ import axios from 'axios';
 
 const getImages = async (query, limit) => {
   const apiKey = '2517208-093ee8e26dcc8dc903fe58900';
+  const params = new URLSearchParams({
+    key: apiKey,
+    q: query,
+    per_page: limit,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+  });
   try {
-    const response = await axios.get("https://pixabay.com/api/?${params}", {
-      params: {
-        key: apiKey,
-        q: query,
-        per_page: limit,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: 'true',
-      },
-    });
+    const response = await axios.get(`https://pixabay.com/api/?${params}`);
     return response.data;
   } catch (error) {
     console.error(error);
